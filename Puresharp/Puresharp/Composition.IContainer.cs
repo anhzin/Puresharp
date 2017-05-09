34627434466257086a82c5fb71c42a7d7a8bd17f
Tony THONG <tony.thong@outlook.com>
2017-05-09 18:45:23 +0200

    -

---
+++ Puresharp/Puresharp/Composition.IContainer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Puresharp
+{
+    static public partial class Composition
+    {
+        public interface IContainer
+        {
+        }
+    }
+}
---
+++ Puresharp/Puresharp/Composition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Puresharp
+{
+    static public partial class Composition
+    {
+        static public void Add(IContainer container)
+        {
+        }
+
+        static public void Remove(IContainer container)
+        {
+        }
+
+        static public T Instance<T>()
+            where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        static public IEnumerable<T> Enumerable<T>()
+            where T : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
