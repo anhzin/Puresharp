be024238235fc4a9dab1c52171daf14a825ecfaf
Tony THONG <tony.thong@outlook.com>
2017-05-12 00:22:15 +0200

    -

---
+++ Puresharp/Puresharp/Startup.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Puresharp
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple=true, Inherited=true)]
+    abstract public partial class Startup : Attribute
+    {
+        abstract public void Run();
+    }
+}
