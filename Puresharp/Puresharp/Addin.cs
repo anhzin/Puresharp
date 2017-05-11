b6f2b5501dbe6bc3daeef7d4098e41093f77b6bb
Tony THONG <tony.thong@outlook.com>
2017-05-12 00:48:53 +0200

    -

---
+++ Puresharp/Puresharp/Addin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Puresharp
+{
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple=true, Inherited=true)]
+    public class Addin : Attribute
+    {
+        public readonly Type Extension;
+        public readonly Type Type;
+
+        public Addin(Type extension, Type type)
+        {
+            this.Extension = extension;
+            this.Type = type;
+        }
+    }
+}
