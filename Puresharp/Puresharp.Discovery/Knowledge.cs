610ff46234a221d6fc1291cebd2dd3289422e3df
Tony THONG <tony.thong@outlook.com>
2017-11-16 08:04:12 +0100

    -

---
+++ Puresharp/Puresharp.Discovery/Knowledge.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Puresharp.Discovery
+{
+    public class Knowledge
+    {
+        static private object m_Handle = new object();
+        static private List<MethodBase> m_Methods = new List<MethodBase>();
+        static private Action<MethodBase> m_Expanded;
+
+        static public event Action<MethodBase> Expanded
+        {
+            add
+            {
+                lock (Knowledge.m_Handle)
+                {
+                    Knowledge.m_Expanded += value;
+                    foreach (var _method in Knowledge.m_Methods) { value(_method); }
+                }
+            }
+            remove
+            {
+                lock (Knowledge.m_Handle)
+                {
+                    Knowledge.m_Expanded -= value;
+                }
+            }
+        }
+
+        static private void Expand(MethodBase method)
+        {
+            lock (Knowledge.m_Handle)
+            {
+                Knowledge.m_Methods.Add(method);
+                Knowledge.m_Expanded?.Invoke(method);
+            }
+        }
+    }
+}
---
+++ Puresharp/Puresharp.Origin/Entry.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Puresharp.Origin
+{
+    [AttributeUsage(AttributeTargets.Constructor | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class Entry : Attribute
+    {
+    }
+}
